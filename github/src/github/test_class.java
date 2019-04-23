fb0b6e0c0ba09aa62112cd7a69376674625dad57
it <it@DESKTOP-KKAQ0RK>
2019-04-23 14:04:45 +0900

    master branch changer a code

---
+++ github/src/github/test_class.java
@@ -0,0 +1,12 @@
+package github;
+
+public class test_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hellow world~!");
+		System.out.println("i'm the master");
+	}
+	
+
+}
